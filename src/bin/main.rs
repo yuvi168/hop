439aaa63b281ffeeb68e80dd0266236819dfa202
yuvi <exghostuser@gmail.com>
2025-09-05 20:45:14 +0530

    feat: I've added some new functionality but its not forbished yet.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,19 +5,31 @@ use std::os::unix::ffi::OsStrExt;
 use regex::Regex;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let zsh_history_path = format!("/home/{}/.zsh_history",
-        users::get_current_username()
-            .unwrap()
-            .to_str()
-            .unwrap()
-    );
-    let source: Vec<u8> = fs::read(zsh_history_path)?;
-    let zsh_history = OsStr::from_bytes(&source[..])
-        .to_string_lossy();
-    let  regex = Regex::new(r"(?m)^([^:]+):([0-9]+):(.+)$").unwrap();
-    for line in zsh_history.lines(){
-        println!("{line}");
-    }
+   let zsh_history_path = format!(
+       "/home/{}/.zsh_history",
+       users::get_current_username()
+           .unwrap()
+           .to_str()
+           .unwrap()
+   );
+   let source: Vec<u8> = fs::read(zsh_history_path)?;
+   let zsh_history = OsStr::from_bytes(&source[..])
+       .to_string_lossy();
+   let  re = Regex::new(r"cd\s*([^:\s]+)").unwrap();
+   let mut paths: Vec<&str> = Vec::new();
+   for (_, [path]) in re.captures_iter(&zsh_history).map(|c| c.extract()) {
+       let path = path.trim();
+       if !paths.contains(&path) {
+           paths.push(&path);
+       }
+   }
+   for path in paths {
+       hop::insert(path.to_string());
+   }
+
+    //finding the path with directory name
+    let path_to_dir = hop::retrieve(String::from("somewhere/i/will/go/one/day"));
+    println!("{path_to_dir}");
     Ok(())
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
-use self::models::NewPath;
+use self::models::{NewPath, Path};
 
 pub mod models;
 pub mod schema;
@@ -14,7 +14,7 @@ fn establish_connection() -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-pub fn create<Sqlite>(new_path: String) {
+pub fn insert(new_path: String) {
     use crate::schema::paths;
     let path = NewPath {path: new_path};
     diesel::insert_into(paths::table).values(&path)
@@ -22,4 +22,15 @@ pub fn create<Sqlite>(new_path: String) {
         .expect("Error saving new path");
 }
 
+pub fn retrieve(directory: String) -> String {
+    use self::schema::{ self, paths::dsl::*};
+    
+    let result = paths
+        .filter(path.like(directory))
+        .select(Path::as_select())
+        .first(&mut establish_connection())
+        .optional()
+        .expect("Couldn't find the path with the queried directory");
+    result.unwrap().path
+}
 
